15a6fe8bd86d6b6f507532fb775c2b985705cac3
Ashley Moon <ashely.r.moon@outlook.com>
2023-02-10 16:35:14 -0800

    Add project files.

---
+++ Pear/App.xaml.cs
@@ -0,0 +1,117 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using H.NotifyIcon;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+using LaunchActivatedEventArgs = Microsoft.UI.Xaml.LaunchActivatedEventArgs;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Pear
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public sealed partial class App : Application
+    {
+        #region Properties
+
+        public TaskbarIcon? TrayIcon { get; private set; }
+        public Window? Window { get; set; }
+        public bool HandleClosedEvents { get; set; } = true;
+
+        #endregion
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            InitializeTrayIcon();
+
+            Peard.Start("APRIL"); // [TODO]: get computer name and put it in here.
+                                  //         the other option is to use a saved identity
+        }
+
+        private void InitializeTrayIcon()
+        {
+            var showHideWindowCommand = (XamlUICommand)Resources["ShowHideWindowCommand"];
+            showHideWindowCommand.ExecuteRequested += ShowHideWindowCommand_ExecuteRequested;
+
+            var exitApplicationCommand = (XamlUICommand)Resources["ExitApplicationCommand"];
+            exitApplicationCommand.ExecuteRequested += ExitApplicationCommand_ExecuteRequested;
+
+            TrayIcon = (TaskbarIcon)Resources["TrayIcon"];
+            TrayIcon.ForceCreate();
+        }
+
+        private void ShowHideWindowCommand_ExecuteRequested(object? _, ExecuteRequestedEventArgs args)
+        {
+            if (Window == null)
+            {
+                Window = new MainWindow();
+                Window.Activate();
+                Window.Closed += (sender, args) =>
+                {
+                    if (HandleClosedEvents)
+                    {
+                        args.Handled = true;
+                        Window.Hide();
+                    }
+                };
+                Window.Show();
+                return;
+            }
+
+            if (Window.Visible)
+            {
+                Window.Hide();
+            }
+            else
+            {
+                Window.Show();
+            }
+        }
+
+        private void ExitApplicationCommand_ExecuteRequested(object? _, ExecuteRequestedEventArgs args)
+        {
+            HandleClosedEvents = false;
+            TrayIcon?.Dispose();
+            Window?.Close();
+
+            // https://github.com/HavenDV/H.NotifyIcon/issues/66
+            if (Window == null)
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ Pear/Peard.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Protection.PlayReady;
+using Windows.Networking.Sockets;
+
+namespace Pear
+{
+    public static class PearDefinitions
+    {
+        public const int DiscoverPort = 17002;
+        public static readonly byte[] Header = new byte[] { 0xF0, 0x9F, 0x8D, 0x90 };
+    }
+
+    public class PearId
+    {
+
+    }
+
+    public class PearDevice
+    {
+        public PearId ID { get; set; } = null!;
+        public int ProtocolVersion { get; set; }
+        public string Name { get; set; } = null!;
+
+        public IEnumerable<byte> ToDackBytes()
+        {
+            var name = Encoding.UTF8.GetBytes(Name);
+            var staticHeader = new byte[]
+            {
+                0xF0, 0x9F, 0x8D, 0x90, // pear header
+                0x01, // version
+                0x01, // message type (DACK)
+                1, 2, 3, 4, // device id [INCOMPLETE]
+                (byte)name.Length, // name length
+
+            };
+            return staticHeader.Concat(name);
+        }
+    }
+
+    public class Peard
+    {
+
+        private static readonly Lazy<Peard> _this = new Lazy<Peard>(() => new Peard());
+        public static Peard Instance {
+            get { return _this.Value; }
+        }
+
+        public static void Start(string name)
+        {
+            Debug.WriteLine($"[peard] starting pear services on device {name}");
+            // trigger constructor
+            _ = Peard.Instance;
+            Peard.Instance._self.Name = name;
+
+            Debug.WriteLine("[peard] starting discovery server");
+            Peard.Instance.InitializeUdp();
+        }
+
+        private PearDevice _self;
+
+        private UdpClient _udp;
+        //private TcpListener _tcp;
+
+        private Peard()
+        {
+            _self = new PearDevice()
+            {
+                ID = new PearId(),
+                ProtocolVersion = 0,
+                Name = "Unknown Device"
+            };
+
+            _udp = new UdpClient(new IPEndPoint(IPAddress.Any, PearDefinitions.DiscoverPort));
+            _udp.EnableBroadcast = true;
+            Debug.WriteLine($"[peard] pear services OK");
+        }
+
+
+        private void InitializeUdp()
+        {
+            var task = Task.Run(() =>
+            {
+                while (true)
+                {
+                    var from = new IPEndPoint(0, 0);
+                    var recvBuffer = _udp.Receive(ref from);
+                    Debug.WriteLine($"[peard][udp] incoming broadcast from {from}");
+                    Debug.WriteLine($"[peard][udp] dump payload: {BitConverter.ToString(recvBuffer)}");
+                    if (recvBuffer[0] != PearDefinitions.Header[0] ||
+                        recvBuffer[1] != PearDefinitions.Header[1] ||
+                        recvBuffer[2] != PearDefinitions.Header[2] ||
+                        recvBuffer[3] != PearDefinitions.Header[3])
+                        continue; // ignore stuff not meant for pear
+                    var version = recvBuffer[4];
+                    var type = recvBuffer[5]; // we can ignore this for now as we dont have any other udp types
+                    Debug.WriteLine($"[peard][udp] received DISC (version {version})");
+                    // only one version exists right now so thats all i care about
+
+                    // next up is the device ID. im going to just use 64 bytes
+                    // well, im using just 4 for testing for now
+                    var id = recvBuffer[6..9];
+
+                    // and i do not care about the rest of it right now
+                    // signing? identities? pfft who needs em
+
+                    var client = new TcpClient();
+                    client.Connect(from);
+                    var stream = client.GetStream();
+                    var data = _self.ToDackBytes().ToArray();
+                    stream.Write(data, 0, data.Length);
+                    Debug.WriteLine("[peard][tcp] wrote DACK");
+                    client.Close();
+                }
+            });
+        }
+
+        private void DISC_Recv()
+        {
+
+        }
+    }
+}
